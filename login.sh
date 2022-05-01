44aad8183fea0937bc90bb77a4a61a61a81af6cc
Scorpio28 <77907586+Scorpio28-Oficial@users.noreply.github.com>
2022-05-01 00:16:07 -0500

    Update login.sh

--- login.sh
+++ login.sh
@@ -21,8 +21,8 @@ echo -e "
 \e[1;32m[\e[1;37m+\e[1;32m] TELEGRAM: https://t.me/Informatic_in_Termux
 \e[0m"
 echo -e""
-read -p $'\e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m CHOOSE A USER:\e[0m ' username
-read -p $'\e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m CHOOSE A PASSWORD:\e[0m ' password
+read -p $'\e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m CHOOSE A USER\e[1;32m ◆ \e[1;32m~ \e[1;37m❯\e[1;36m❯\e[1;32m❯\e[0m ' username
+read -p $'\e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m CHOOSE A PASSWORD\e[1;32m ◆ \e[1;32m~ \e[1;37m❯\e[1;36m❯\e[1;32m❯\e[0m ' password
 cd
 cd ../usr/etc/
 rm -rf motd
@@ -77,8 +77,8 @@ echo -e ""
 sleep 0.1
 echo -e "\e[1;31m            >>>\e[1;32m ENTER YOUR ACCESS CODE\e[1;31m <<<\e[0m"
 read -p $'\e[1;30m
-\e[1;30m                 \e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m USERNAME\e[1;31m :\e[0;30m ' user
-read -s -p $'\e[1;30m                 \e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m PASSWORD\e[1;31m :\e[0;30m ' pass
+\e[1;30m           \e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m USERNAME\e[1;31m \e[1;32m ◆ \e[1;32m~ \e[1;37m❯\e[1;36m❯\e[1;32m❯\e[0;30m ' user
+read -s -p $'\e[1;30m           \e[1;31m[\e[1;32m+\e[1;31m]\e[1;32m PASSWORD\e[1;31m \e[1;32m ◆ \e[1;32m~ \e[1;37m❯\e[1;36m❯\e[1;32m❯\e[0;30m ' pass
 if [[ \$pass == $password && \$user == $username ]]; then
 echo -e "\e[1;32m\e[0m"
 echo -e "\e[1;32m\e[0m"
